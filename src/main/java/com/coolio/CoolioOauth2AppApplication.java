9199d04cd5a84924fa9d8634dcca222d34c426b4
Aseem Savio <aseemsavio3@gmail.com>
2019-03-11 09:16:25 +0530

    Initial commit

---
+++ src/main/java/com/coolio/CoolioOauth2AppApplication.java
@@ -0,0 +1,13 @@
+package com.coolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolioOauth2AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolioOauth2AppApplication.class, args);
+	}
+
+}
