c8ead7876bc3081dda9e3df5d426748f94e77615
Aseem Savio <aseemsavio3@gmail.com>
2019-04-19 12:09:15 +0530

    Eureka Client addition

--- src/main/java/com/coolio/CoolioOauth2Application.java
+++ src/main/java/com/coolio/CoolioOauth2Application.java
@@ -2,8 +2,10 @@ package com.coolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CoolioOauth2Application {
 
 	public static void main(String[] args) {
